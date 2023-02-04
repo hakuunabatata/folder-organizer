27a53ca232d1619c8a06d5d6a0a3a0b38d8ed196
hakuunabatata <emachadomartins1999@gmail.com>
2023-02-04 12:36:52 -0300

    create in.py

---
+++ out.py
@@ -0,0 +1,46 @@
+import os
+
+basePath = os.getcwd()
+
+
+def createPath(paths):
+    return '/'.join(paths)
+
+
+def desorganizeFolder(path):
+
+    print(f'...organizing {path}')
+
+    os.chdir(path)
+
+    files = os.listdir(path)
+
+    for file in files:
+        filePath = createPath([path, file])
+
+        if ('.' in file):
+            print(f'...analysing file {file}')
+            name, extension = file.split('.')
+
+            if (extension not in os.listdir(path)):
+                os.mkdir(extension)
+                print(f'...creating dir {extension} in {path}')
+
+            os.rename(filePath,
+                      createPath([path, extension, file]))
+
+            print(f'...moving {name} to {extension} folder')
+        else:
+            print(f'...analysing folder {file}')
+            if (os.listdir(filePath) == []):
+                os.rmdir(filePath)
+                print(f'...deleting empty folder {file}')
+
+            else:
+                desorganizeFolder(filePath)
+                os.chdir(path)
+
+
+path = createPath([basePath, input('Qual diretório quer desorganizar?\n\n')])
+
+desorganizeFolder(path)
